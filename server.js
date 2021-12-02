fa72a8019e0186adaae4a8d4dbbd061a51525996
Snake <chaldrich24@gmail.com>
2021-12-02 17:59:12 -0500

    add user routes

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const sequelize = require('./config/connection');
 const express = require('express');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = 3001;
 
-sequelize.sync({ force: true }).then(() => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`You are now connected to port ${PORT}`);
     })
